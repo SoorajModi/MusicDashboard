e71631858824dce3e45ff4901c116150145e244d
SoorajModi <soomodi@gmail.com>
2022-05-02 19:24:23 -0400

    Add DaisyUI + Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-link normal-case text-xl text-base-content hover:no-underline" href="/">FilmHub Music</a>
+      </div>
+      <div className="flex-none pr-3">
+        <div className="form-control">
+          <input type="text" placeholder="Search" className="input input-bordered" />
+        </div>
+      </div>
     </div>
   );
 }
