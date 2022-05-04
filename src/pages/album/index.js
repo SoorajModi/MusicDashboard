a96e5001a9a4416c9ea2d8700a45d1241fd1d305
SoorajModi <soomodi@gmail.com>
2022-05-03 23:34:04 -0400

    Add message if album not found

--- src/pages/album/index.js
+++ src/pages/album/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import uniqid from 'uniqid';
 import { getAlbums } from '../../api/albums';
 
 function Album({ albums, requestAlbums }) {
@@ -16,17 +17,22 @@ function Album({ albums, requestAlbums }) {
     }
   }, [albums]);
 
-  return (
-    <div className="flex flex-row w-full h-96 justify-center items-center space-x-3 my-20">
-      <img src={album.artworkUrl100} alt={`${album.name} album artwork`} className="w-96" />
-      <div className="text-center max-w-lg">
-        <div className="text-3xl font-bold">{album.name || 'N/A'}</div>
-        <div className="text-xl">{album.artistName || 'N/A'}</div>
-        <div className="text-sm space-x-2">{album.genres && album.genres.map((g) => <span>{g.name}</span>)}</div>
-        <div className="text-sm">{album.releaseDate && album.releaseDate.toString().slice(0, 4)}</div>
+  return Object.keys(album).length !== 0
+    ? (
+      <div className="flex flex-row w-full h-96 justify-center items-center space-x-3 my-20">
+        <img src={album.artworkUrl100} alt={`${album.name} album artwork`} className="w-96" />
+        <div className="text-center max-w-lg">
+          <div className="text-3xl font-bold">{album.name}</div>
+          <div className="text-xl">{album.artistName}</div>
+          <div className="text-sm space-x-2">{album.genres.map((g) => <span key={uniqid()}>{g.name}</span>)}</div>
+          <div className="text-sm">{album.releaseDate.toString().slice(0, 4)}</div>
+        </div>
       </div>
-    </div>
-  );
+    ) : (
+      <div className="flex justify-center items-center w-full">
+        Album not found
+      </div>
+    );
 }
 
 const mapStateToProps = (state) => ({
