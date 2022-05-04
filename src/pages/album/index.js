d91a9fa5ba30325967e6cb285f06e6237cb54167
SoorajModi <soomodi@gmail.com>
2022-05-03 23:11:35 -0400

    Add api request when album info is missing

--- src/pages/album/index.js
+++ src/pages/album/index.js
@@ -4,22 +4,22 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getAlbums } from '../../api/albums';
 
-function Album({ albums }) {
+function Album({ albums, requestAlbums }) {
   const { id } = useParams();
   const [album, setAlbum] = useState({});
 
   useEffect(() => {
     if (albums && albums.results) {
       setAlbum(albums.results.find((item) => item.id === id));
+    } else {
+      requestAlbums();
     }
   }, [albums]);
 
   return (
     <div className="flex flex-row w-full h-96 justify-center items-center space-x-3 my-20">
-      <div>
-        <img src={album.artworkUrl100} alt={`${album.name} album artwork`} className="w-96" />
-      </div>
-      <div className="text-center">
+      <img src={album.artworkUrl100} alt={`${album.name} album artwork`} className="w-96" />
+      <div className="text-center max-w-lg">
         <div className="text-3xl font-bold">{album.name || 'N/A'}</div>
         <div className="text-xl">{album.artistName || 'N/A'}</div>
         <div className="text-sm space-x-2">{album.genres && album.genres.map((g) => <span>{g.name}</span>)}</div>
