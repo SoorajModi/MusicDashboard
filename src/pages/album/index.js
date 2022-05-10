88fda32a62ebf21e0987bf4ba8316c2aec1d593d
Sooraj <soomodi@gmail.com>
2022-05-09 21:05:16 -0400

    Fixes (#14)
    * Refactor album component
    
    * Update tab icon
    
    * Add navbar pseudocode

--- src/pages/album/index.js
+++ src/pages/album/index.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import uniqid from 'uniqid';
 import { getAlbums } from '../../api/albums';
+import NotFound from './components/notFound';
+import Found from './components/found';
 
 function Album({ albums, requestAlbums }) {
   const { id } = useParams();
@@ -17,22 +18,15 @@ function Album({ albums, requestAlbums }) {
     }
   }, [albums]);
 
-  return Object.keys(album).length !== 0
-    ? (
-      <div className="flex flex-row w-full h-96 justify-center items-center space-x-3 my-20">
-        <img src={album.artworkUrl100} alt={`${album.name} album artwork`} className="w-96" />
-        <div className="text-center max-w-lg">
-          <div className="text-3xl font-bold">{album.name}</div>
-          <div className="text-xl">{album.artistName}</div>
-          <div className="text-sm space-x-2">{album.genres.map((g) => <span key={uniqid()}>{g.name}</span>)}</div>
-          <div className="text-sm">{album.releaseDate.toString().slice(0, 4)}</div>
-        </div>
-      </div>
-    ) : (
-      <div className="flex justify-center items-center w-full">
-        Album not found
-      </div>
-    );
+  return isAlbumFound(album)
+    ? <Found album={album} />
+    : <NotFound />;
+}
+
+function isAlbumFound(obj) {
+  return obj
+    && Object.getPrototypeOf(obj) === Object.prototype
+    && Object.keys(obj).length !== 0;
 }
 
 const mapStateToProps = (state) => ({
